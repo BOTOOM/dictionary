71f7354be55f5f2f43c641bab129a51cfbc61f39
BOTOOM <eddiazr@correo.udistrital.edu.co>
2021-04-13 12:47:57 -0500

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useState, FC } from 'react'
+import NavBar from './components/NavBar';
 
 import './App.css';
 
 const  App:FC = () => {
   return (
-    <main>
-      holi
-    </main>
+    <div className="contenedor_general">
+      <NavBar/>
+    </div>
   );
 }
 
