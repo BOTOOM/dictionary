6a7e8b7cef340beae48a7ba9b16f58ff13fcbc8a
BOTOOM <eddiazr@correo.udistrital.edu.co>
2021-04-18 17:33:11 -0500

    peticion y envio a padre

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,56 @@
-import React, { useState, FC } from 'react'
+import React, { useState,useEffect, FC } from 'react'
 import NavBar from './components/NavBar';
 import SearchCard from './components/searchCard';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import axios from 'axios';
 
 import './App.css';
 
 const App: FC = () => {
+
+  const [resultSearch, setResultSearch] = useState({})
+  const [word, setWord] = useState('');
+
+  useEffect(() => {
+
+    const getDictionary = async (palabra) => {
+      console.log('get dictionary',palabra)
+      try {
+        if (palabra !== '') {
+          const response = await axios.request({
+            method: 'GET',
+            baseURL: 'https://lingua-robot.p.rapidapi.com/language/v1',
+            url: `/entries/en/${palabra}`,
+            headers: {
+              'x-rapidapi-key': `${process.env.api_key}`,
+              'x-rapidapi-host': 'lingua-robot.p.rapidapi.com'
+            }
+          });
+          console.log(response.data)
+        }
+        // setResultSearch(respo)
+      } catch (error) {
+        console.log('ÉRROR',error)
+      }
+      
+
+    }
+    // // return () => {
+    // //   cleanup
+    // // }
+    getDictionary(word);
+  }, [word])
+  
   return (
     <div className="contenedor_general">
       <NavBar />
       <Container fixed>
-        <SearchCard/>
+        <SearchCard onTipeWord={(value) =>  {
+          console.log(value)
+          setWord(value);
+          // getDictionary()
+          } } />
       </Container>
     </div>
   );
--- src/components/searchCard/index.tsx
+++ src/components/searchCard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
+// import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -10,7 +10,7 @@ import TextField from '@material-ui/core/TextField';
 const useStyles = makeStyles({
     root: {
         minWidth: 275,
-        marginTop: 50
+        marginTop: 20
     },
     texto: {
         margin: '0 2px',
@@ -28,27 +28,30 @@ const useStyles = makeStyles({
 
 interface State {
     word: string;
-  }
+}
 
-
-export default function SearchCard() {
+const SearchCard = ({ onTipeWord }) => {
     const classes = useStyles();
-    const bull = <span className={classes.bullet}>•</span>;
+    // const bull = <span className={classes.bullet}>•</span>;
 
     const [values, setValues] = React.useState<State>({
         word: '',
-      });
+    });
 
-      const _handleKeyDown = (e:any) => {
+    const _handleKeyDown = (e: any) => {
         if (e.key === 'Enter') {
-          console.log('do validate', values.word);
+            console.log('do validate', values.word);
+            sendWord();
         }
-      }
+    }
 
-      const handleChange = (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
         setValues({ ...values, [prop]: event.target.value });
-      };
-    
+    };
+
+    const sendWord = () => {
+        onTipeWord(values.word);
+    }
 
     return (
         <Card className={classes.root}>
@@ -57,12 +60,14 @@ export default function SearchCard() {
                 <Typography variant="h5" component="h2">
                     What word do you want to search for?
         </Typography>
-                <TextField className={classes.texto} id="outlined-basic" label="Type your word" variant="outlined"
-                onChange={handleChange('word')}  onKeyDown={_handleKeyDown} value={values.word} />
-                <Button className={classes.boton} size="large" variant="outlined" color="primary">
+                <TextField size="small" className={classes.texto} id="outlined-basic" label="Type your word" variant="outlined"
+                    onChange={handleChange('word')} onKeyDown={_handleKeyDown} value={values.word} />
+                <Button className={classes.boton} size="large" variant="outlined" color="primary" onClick={sendWord}>
                     Search
                 </Button>
             </CardContent>
         </Card>
     );
 }
+
+export default SearchCard;
