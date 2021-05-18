37d22e3d242eeecaebd77f0492c36c457f0c88fb
BOTOOM <eddiazr@correo.udistrital.edu.co>
2021-05-18 16:00:48 -0500

    intento 2 hashrouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter} from 'react-router-dom'
 
-ReactDOM.render(<App />,                                    
+ReactDOM.render(
+<HashRouter
+  basename={''}
+  // getUserConfirmation={optionalFunc}
+  // hashType={optionalString}
+>
+<App />
+</HashRouter>
+,                                    
   document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
